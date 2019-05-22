370d7e11eb1ca12696acf7a4dd3238484d754f63
ryden <ryferr8122@gmail.com>
2019-05-22 18:42:05 -0500

    stylized sidebar

--- client/components/Sidebar/SidebarHeader.jsx
+++ client/components/Sidebar/SidebarHeader.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 const SidebarHeader = () => {
   return(
     <div id="Sidebar-Header">
+      <div>
+        {/* <img src="../client/assets/logo.png"/> */}
+      </div>
+
       <div id="Brand">Ryan Ferrer</div>
       <div id="Title">Full Stack Software Developer</div>
     </div>
